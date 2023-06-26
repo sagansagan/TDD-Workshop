72a63362ca3c86fb529d685043d7f1c01038588d
Saga Andersson <sagander@hotmail.com>
2023-06-27 01:10:18 +0200

    added tests and functions

--- WordLib/Worder.cs
+++ WordLib/Worder.cs
@@ -4,7 +4,37 @@
     {
         public static bool IsPalindrome(string word)
         {
+            if (string.IsNullOrEmpty(word))
+                throw new ArgumentException("Word cannot be null or empty.");
 
+            string palindrome = new string(word.ToLower().ToCharArray().Reverse().ToArray());
+
+            if (word.Equals(palindrome))
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+        public static int CountLetters(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return 0;
+            }
+
+            int letterCount = 0;
+
+            foreach (char l in word)
+            {
+                if (char.IsLetter(l))
+                {
+                    letterCount++;
+                }
+            }
+
+            return letterCount;
         }
 
     }
--- WordLibTests/UnitTest1.cs
+++ WordLibTests/UnitTest1.cs
@@ -6,12 +6,49 @@ namespace WordLibTests
     public class UnitTest1
     {
         [TestMethod]
-        public void PalindromeTest()
+        public void PalindromeTestTrue()
         {
-            bool result = Worder.IsPalindrome("madam");
+            bool result = Worder.IsPalindrome("sirap i paris");
             Assert.IsTrue(result);
         }
 
+        [TestMethod]
+        public void PalindromeTestFalse()
+        {
+            bool result = Worder.IsPalindrome("senap i paris");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ThrowsException()
+        {
+            //arrange
+            string input = "";
+
+            //Act & assert
+            Assert.ThrowsException<ArgumentException>(() => Worder.IsPalindrome(input));
+        }
+
+        [TestMethod]
+        public void CountLetters_EmptyString()
+        {
+            string word = "";
+
+            int result = Worder.CountLetters(word);
+
+            Assert.AreEqual(0, result);
+
+        }
+
+        [TestMethod]
+        public void CountLetters_CountTest()
+        {
+            string word = "Coca-cola12345";
+
+            int result = Worder.CountLetters(word);
+
+            Assert.AreEqual(8, result);
+        }
 
     }
 }
